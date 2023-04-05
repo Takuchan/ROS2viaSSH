00bb4fa9e280e8cd73244d465869f80eac968ca8
Takuchan <takuchanapp@gmail.com>
2023-04-05 16:27:13 +0900

    SSH接続を完了した後、コマンドを実行できるようになった

--- app/src/main/java/com/takuchan/ros2viassh/MainActivity.java
+++ app/src/main/java/com/takuchan/ros2viassh/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSch;
@@ -15,16 +16,25 @@ import com.jcraft.jsch.Session;
 public class MainActivity extends AppCompatActivity {
 
     Button connectButton;
+    EditText addressEdit,passwordEdit,userNameEdit;
+
+    String address,password,username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         connectButton = (Button)findViewById(R.id.button);
 
+        addressEdit = (EditText)findViewById(R.id.address_edittext);
+        passwordEdit = (EditText)findViewById(R.id.password_edittext);
+        userNameEdit = (EditText)findViewById(R.id.username_edittext);
+
+
+
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SSHConnection task = new SSHConnection("","","");
+                SSHConnection task = new SSHConnection(userNameEdit.getText().toString(),passwordEdit.getText().toString(),addressEdit.getText().toString());
                 task.execute();
             }
         });
--- app/src/main/java/com/takuchan/ros2viassh/SSHConnection.java
+++ app/src/main/java/com/takuchan/ros2viassh/SSHConnection.java
@@ -3,6 +3,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.jcraft.jsch.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class SSHConnection extends AsyncTask<Void,Void,Void> {
     private String user;
     private String password;
@@ -26,13 +32,29 @@ public class SSHConnection extends AsyncTask<Void,Void,Void> {
             session.connect();
 
             // SSH接続が成功した場合の処理
-            channel = session.openChannel("shell");
-            channel.setInputStream(System.in);
-            channel.setOutputStream(System.out);
-            channel.connect();
+//            channel = session.openChannel("shell");
+//            channel.setInputStream(System.in);
+//            channel.setOutputStream(System.out);
+//            channel.connect();
+
+            ChannelExec execChannel = (ChannelExec) session.openChannel("exec");
+            InputStream inputStream = execChannel.getInputStream();
+            execChannel.setCommand("ls");
+            execChannel.connect();
+
+            // 結果の読み込みとTextViewへの表示
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+            Log.d("ログの情報",stringBuilder.toString());
 
         } catch (JSchException e) {
             // SSH接続が失敗した場合の処理
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
